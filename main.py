8f88b73d57f3fa1adb1f3bbeba4591aa6d2304fe
privacy <115932841+privacy777@users.noreply.github.com>
2023-01-25 10:02:50 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+
+dcoded = ""
+with open("text.txt",'r') as t:
+    for line in t:
+        for word in line.split():
+            a = chr(int(word))
+            dcoded += str(a)
+print(dcoded)
+            
+
+            
